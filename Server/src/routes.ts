29437825c695a0033f28f386c881bb431bffe0d5
Mario Lioli Pacheco <pacheco.lioli@gmail.com>
2022-05-17 15:07:22 -0300

    Status of error

--- Server/src/routes.ts
+++ Server/src/routes.ts
@@ -7,25 +7,20 @@ export const routes = express.Router();
 
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
-  try {
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
-    const nodemailerMailAdapter = new NodemailerMailAdapter();
 
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-      prismaFeedbacksRepository,
-      nodemailerMailAdapter
-    );
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  const nodemailerMailAdapter = new NodemailerMailAdapter();
 
-    await submitFeedbackUseCase.execute({
-      type,
-      comment,
-      screenshot,
-    });
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository,
+    nodemailerMailAdapter
+  );
 
-    return res.status(201).send();
-  } catch (err) {
-    console.error(err);
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
+  });
 
-    return res.status(404).send();
-  }
+  return res.status(201).send();
 });
