0402f4f1f366ca623467b684274ac433590573e0
Mario Lioli Pacheco <pacheco.lioli@gmail.com>
2022-05-17 12:17:47 -0300

    First commit Server

---
+++ Server/src/server.ts
@@ -0,0 +1,21 @@
+import cors from "cors";
+import express from "express";
+import { routes } from "./routes";
+
+const app = express();
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET = Buscar informações
+// POST = Cadastrar informações
+// PUT = Atualizar informações de uma entidade
+// PATCH = Atualizar informações de uma única entidade, ex: aceita receber algo, sim ou não com
+// DELETE = Deletar uma informação
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log("HTTP server running!");
+});
